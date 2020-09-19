91522e8449c216ab9cf7ca62fcdd0f48038b582e
saorim10 <marcelo.saorim@gmail.com>
2020-09-19 11:54:31 -0300

    Adicionando rotas no React Native

--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -2,43 +2,47 @@ import React from 'react';
 import { FontAwesome5 as Icon} from '@expo/vector-icons';
 import { Text, StyleSheet, View, Image, Alert } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import Header from '../../components/Header';
 
 const Home = () => {
+  const navigation = useNavigation(); 
 
-    const handleOnPress = () => {
-        Alert.alert('Você clicou!!!')
-    }
+  const handleOnPress = () => {
+    navigation.navigate('CreateRecord');
+  }
 
-    return (
-        <>
-            <View style={styles.container}>
-                <Image
-                    source={require('../../assets/gamer.png')}
-                    style={styles.gamerImage}
-                />
-                <Text style={styles.title}>Vote Agora!</Text>
-                <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito!</Text>
-            </View>
-            <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={handleOnPress}>
-                    <Text style={styles.buttonText}>
-                        Coletar Dados
+  return (
+    <>
+      <Header />
+      <View style={styles.container}>
+        <Image
+          source={require('../../assets/gamer.png')}
+          style={styles.gamerImage}
+        />
+        <Text style={styles.title}>Vote Agora!</Text>
+        <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito!</Text>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleOnPress}>
+          <Text style={styles.buttonText}>
+            Coletar Dados
                     </Text>
-                    <View style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name="chevron-right" color="#FFF" size={25} />
-                        </Text>
-                    </View>
-                </RectButton>
-            </View>
-        </>
-        
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="chevron-right" color="#FFF" size={25} />
+            </Text>
+          </View>
+        </RectButton>
+      </View>
+    </>
 
 
 
 
-        
-    )
+
+
+  )
 };
 
 const styles = StyleSheet.create({
